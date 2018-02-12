9193dbf92a70ccc5e37f0173d4b5ab8e8d963385
wxp123321 <wangxupu@gmail.com>
2018-02-12 22:47:14 +0800

    头部

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    helloworld
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/variable.styl"
+
+  #app
+    color : $color-theme
+</style>
