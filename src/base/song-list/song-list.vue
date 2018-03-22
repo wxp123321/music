711fdf05317a22594d111342780c19daaefb7360
1648557071@qq.com <1648557071@qq.com>
2018-03-22 22:27:14 +0800

    3-22

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="song-list">
     <ul>
-      <li v-for="song in songs" class="item">
+      <li @click="selectItem(song,index)" v-for="(song,index) in songs" class="item">
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
@@ -12,19 +12,22 @@
 </template>
 
 <script type="text/ecmascript-6">
- export default {
-   props: {
-     songs: {
-       type: Array,
-       default: []
-     }
-   },
-   methods: {
-     getDesc(song) {
-       return `${song.singer}·${song.album}`
-     }
-   }
- }
+  export default {
+    props: {
+      songs: {
+        type: Array,
+        default: []
+      }
+    },
+    methods: {
+      getDesc(song) {
+        return `${song.singer}·${song.album}`
+      },
+      selectItem(item, index) {
+        this.$emit('select', item, index)
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="player">
-    <div class="normal-player">
+    <div class="normal-player" v-show="fullScreen)">
       播放器
     </div>
-    <div class="mini-player"></div>
+    <div class="mini-player" v-show="!fullScreen)"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  import {mapGetters} from 'vuex'
+
+  export default {
+    computed: {
+      ...mapGetters([
+        'fullScreen',
+        'playlist'
+      ])
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
